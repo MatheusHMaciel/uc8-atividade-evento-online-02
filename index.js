35881ff6386c6c9a459a918cf41f23d426538048
matheus <caiocto@gmail.com>
2021-09-11 03:36:46 -0300

    corrigido

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let cadastroevento=[]
-let idade=25
-let datadoevento=10
+let idade=19
+let datadoevento=5
 let podecadastrar=true
 let qtdparticipantes=50
+
 
 
 if (datadoevento>10){
@@ -10,17 +11,17 @@ console.log ('data do evento nao coincide com a atual')
 podecadastrar=false
 }
 
-if (idade<18){
+if (idade>18){
 console.log('idade menor do que permitido cadastro nao realizado')
 podecadastrar=false
 }
 
-if (qtdparticipantes<100){
+if (qtdparticipantes<50){
 console.log('cadastro nao permitido quantidade execedida')
 podecadastrar=false
 }
 
-if (podeCadastrar=true){
+if (podecadastrar){
     cadastroevento.push(cadastroevento)
     console.log('cadastramento efetuado com sucesso')
     } else{
@@ -28,6 +29,6 @@ if (podeCadastrar=true){
     }
 
     console.log('cadastroevento ')
-for (let i=0;i<cadastroevento.idade.qtdparticipantes;i){
+for (let i=0;i<cadastroevento.idade;i){
 console.log(cadastroevento[i])
 }
